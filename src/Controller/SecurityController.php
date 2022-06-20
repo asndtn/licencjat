e8c27521cbae0ad734cc40d5a40d38b5a24324a9
Wiktoria <wiktoria.lubzinska@gmail.com>
2022-06-20 22:00:32 +0200

    Added UserService & UserRepo

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use App\Repository\UserRepository;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * User service.
+     */
+    private UserService $userService;
+
+    /**
+     * Translator.
+     */
+    private TranslatorInterface $translator;
+
+    /**
+     * Constructor
+     *
+     * @param UserService $userService User service
+     * @param TranslatorInterface $translator Translator
+     */
+    public function __construct(UserService $userService, TranslatorInterface $translator)
+    {
+        $this->userService = $userService;
+        $this->translator = $translator;
+    }
+
+    /**
+     * Login action.
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     *
+     * @return Response HTTP Response
+     */
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -22,6 +59,39 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @param Request $request
+     * @param UserPasswordHasherInterface $passwordHasher
+     * @param UserRepository $userRepository
+     * @return Response
+     */
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, UserRepository $userRepository): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $user->getRoles();
+            $userRepository->save($user);
+
+            $this->addFlash(
+                'success',
+                $this->translator->trans('message.registered_successfully')
+            );
+
+            return $this->redirectToRoute('input_index');
+        }
+
+        return $this->render(
+            'security/register.html.twig',
+        ['form' => $form->createView()]
+        );
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
